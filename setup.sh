6e726bbc7cb79bf679407abc8d0ab30aaf1f9aea
Ronen Hilewicz <ronen.hilewicz@enigma.com>
2021-08-12 16:51:27 -0400

    Cleaning up setup.sh and adding homebrew formulae

--- setup.sh
+++ setup.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd)
+DOTFILES="ackrc vimrc zshrc gitconfig fzf_funcs.zsh"
+HOMEBREW_INSTALLER=https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+HOMEBREW_FORMULAE="vim antigen ctags fzf pyenv pyenv-virtualenv"
 
 # Create symbolic links
-ln -s $SCRIPT_DIR/ackrc ~/.ackrc
-ln -s $SCRIPT_DIR/vimrc ~/.vimrc
-ln -s $SCRIPT_DIR/zshrc ~/.zshrc
-ln -s $SCRIPT_DIR/gitconfig ~/.gitconfig
-ln -s $SCRIPT_DIR/fzf_funcs.zsh ~/.fzf_funcs.zsh
+for dotfile in $DOTFILES; do
+    echo "Creating symlink for ~/.$dotfile."
+    ln -s $SCRIPT_DIR/$dotfile ~/.$dotfile
+done
+
+# Install homebrew
+echo "Installing Homebrew from $HOMEBREW_INSTALLER"
+/bin/bash -c "$(curl -fsSL $HOMEBREW_INSTALLER)"
+
+# Pour core formulae
+brew install $HOMEBREW_FORMULAE
 
 # copy VIM ALE plugin configuration
 mkdir ~/.vim/ftplugin
 cp $SCRIPT_DIR/ftplugin/* ~/.vim/ftplugin/
-
-# Pour core formulae
-brew install vim antigen ctags fzf pyenv pyenv-virtualenv
